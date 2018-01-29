2bce58ae94576e8ec1dab711c293d8e30c538385
Jonathan Hermsen <jonathanhermsen@gmail.com>
2018-01-29 00:16:26 -0600

    Add WYGIWiki plugin for CKEditor

--- Default.aspx
+++ Default.aspx
@@ -9,6 +9,12 @@
 	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" />
 
 	<title>WYGIWiki</title>
+
+	<style>
+		.cke_dialog_ui_input_textarea {
+			height: 330px !important;
+		}
+	</style>
 </head>
 <body>
 	<nav class="navbar navbar-dark bg-primary">
@@ -24,6 +30,7 @@
 	<script type="text/javascript" src="https://cdn.jsdelivr.net/npm/jquery@3.3.1/dist/jquery.min.js"></script>
 	<script type="text/javascript" src="https://cdn.jsdelivr.net/npm/ckeditor@4.8.0/ckeditor.js"></script>
 	<script type="text/javascript">
+		CKEDITOR.plugins.addExternal("wygiwiki", "/plugins/wygiwiki/", "plugin.min.js");
 		jQuery.ajaxSetup({
 			type: "POST",
 			dataType: "json",
@@ -41,9 +48,12 @@
 			DOM.input = CKEDITOR.replace("editor", {
 				format_tags: "p;h2;h3;h4",
 				removeButtons: "",
+				customValues: { convert_url: "/api/content.svc/ConvertContent" },
+				extraPlugins: "wygiwiki",
 				skin: "bootstrapck,https://cdn.jsdelivr.net/gh/Kunstmaan/BootstrapCK4-Skin@1.0.0/skins/bootstrapck/",
 				toolbar: [
-					[ "Undo", "Redo" ],
+					[ "Wygiwiki" ],
+					[ "Undo", "Redo"],
 					[ "Cut", "Copy", "Paste", "PasteText" ],
 					[ "Format", "Bold", "Italic", "-", "RemoveFormat" ],
 					[ "BulletedList", "NumberedList", "Outdent", "Indent" ],
---
+++ plugins/wygiwiki/plugin.js
@@ -0,0 +1,84 @@
+﻿(function () {
+	"use strict";
+
+	var DOM = {};
+
+	const dialogTitle = "Convert markup";
+
+		// toWiki controls
+	var wikiOutput = { type: "textarea", id: "wikiOutput", onLoad: setReadOnly },
+		submitHTML = { type: "button", id: "submitHTML", label: "Convert to MediaWiki", onClick: convertToWiki },
+		// toHTML controls
+		wikiInput = { type: "textarea", id: "wikiInput" },
+		submitWiki = { type: "button", id: "submitWiki", label: "Convert to HTML", onClick: convertToHTML };
+
+	function setReadOnly () { this.getInputElement().setAttribute("readOnly", true); }
+	function convertToWiki () { convertContent("html"); }
+	function convertToHTML () { convertContent("wiki"); }
+	function convertContent (currentFormat) {
+		var editor = DOM.dialog.editor,
+			url = editor.config.customValues.convert_url,
+			data = { obj: {} },
+			rawContent = "";
+
+		if (!url || !currentFormat) return;
+
+		if (currentFormat === "html") data.obj.htmlContent = editor.getData();
+		if (currentFormat === "wiki") data.obj.wikiContent = DOM.dialog.contents.toHTML.wikiInput.getValue();
+
+		if (!data.obj.htmlContent && !data.obj.wikiContent) {
+			if (currentFormat === "html") {
+				alert("Please enter something into the editor first.");
+
+				CKEDITOR.dialog.getCurrent().hide();
+			}
+			if (currentFormat === "wiki") {
+				alert("Please enter something into the textbox first.");
+
+				DOM.dialog.contents.toHTML.wikiInput.focus();
+			}
+
+			return;
+		}
+
+		jQuery.ajax({
+			url: url,
+			data: JSON.stringify(data)
+		}).done(function (data) {
+			var value = data.ConvertContentResult;
+
+			if (currentFormat === "html") DOM.dialog.contents.toWiki.wikiOutput.setValue(value);
+			if (currentFormat === "wiki") {
+				editor.setData(value);
+
+				CKEDITOR.dialog.getCurrent().hide();
+			}
+		});
+	}
+	CKEDITOR.plugins.add("wygiwiki", {
+		icons: "wygiwiki",
+		init: function (editor) {
+			editor.addCommand("convertMarkup", new CKEDITOR.dialogCommand("wygiwikiDialog"));
+			editor.ui.addButton("Wygiwiki", {
+				label: dialogTitle,
+				command: "convertMarkup"
+			});
+		}
+	});
+	CKEDITOR.dialog.add("wygiwikiDialog", function (editor) {
+		return {
+			title: dialogTitle,
+			contents: [{
+				id: "toWiki",
+				label: "To MediaWiki",
+				elements: [submitHTML, wikiOutput]
+			}, {
+				id: "toHTML",
+				label: "From MediaWiki",
+				elements: [wikiInput, submitWiki]
+			}],
+			onLoad: function () { DOM.dialog = this._; },
+			buttons: []
+		}
+	});
+} ());
