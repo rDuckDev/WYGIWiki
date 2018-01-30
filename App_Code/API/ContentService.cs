e9746531e92dc14e438e22c101f4f8e372018380
Jonathan Hermsen <jonathanhermsen@gmail.com>
2018-01-29 20:59:35 -0600

    Have C# address temp directory

--- App_Code/API/ContentService.cs
+++ App_Code/API/ContentService.cs
@@ -32,8 +32,11 @@ public class ContentService : IContentService
 		}
 
 		string fileName = String.Format("{0}.txt", Guid.NewGuid().ToString());
+		string fileDirectory = HttpContext.Current.Server.MapPath("~/temp");
 		string filePath = HttpContext.Current.Server.MapPath(String.Format("~/temp/{0}", fileName));
 		string[] input = rawInput.Split(new string[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
+
+		if (!Directory.Exists(fileDirectory)) Directory.CreateDirectory(fileDirectory);
 
 		File.WriteAllLines(filePath, input);
 
